f802d299d977ec915319d59798e9f66420160fb1
selkamand <sam.elkamand@gmail.com>
2026-06-01 11:42:36 +1000

    feat: add basic count behaviour

--- R/count.R
+++ R/count.R
@@ -1,3 +1,128 @@
-bcount <- function(x, columns){
+#' Count
+#'
+#' Tally the frequency of each level in a column
+#' (or combination of levels in several column).
+#'
+#' @param columns names of columns whole levels to tally (character vector).
+#' @param sort sort by tallies in descending order.
+#' If \code{FALSE} table is sorted by columns (flag)
+#' @param name name of column describing frequencies.
+#' Where \code{name} clashes with \code{columns} it will be
+#' dot-prefixed until clash is removed. See details (string).
+#' @param drop exclude zero-counts from factor levels not present in data.
+#' @return a data.frame describing counts per level.
+#'
+#' @examples
+#' # Create a sample data frame
+#' df <- data.frame(
+#'   x = 1:5,
+#'   y = letters[1:5],
+#'   z = rnorm(5)
+#' )
+#'
+#' # Count columns 'x' and 'z'
+#' count(df, c("x", "z"))
+#'
+#' # Set name of tally column
+#' count(df, c("x", "z"), name = "tally")
+#'
+#' @details
+#' It is not guaranteed that the tally column name defined by `name`
+#' will be the true name in the resulting data.frame.
+#' If the `name` argument clashes with a column in `columns` then name will
+#' be prefixed with '.' until unique.
+#'
+#' The fact we cannot guarantee tally column name means there are only two safe ways extract this tally column.
+#' 1. Extract the final column of the count data.frame
+#' 2. Lookup the 'n' attribute of the count data.frame which describes the true name of the tally column.
+#'
+#' @export
+count <- function(.data, columns, sort = FALSE, name = "n", drop = TRUE) {
+  # Assertions
+  if (!is.data.frame(.data)) {
+    stop("'.data' must be a data.frame")
+  }
 
+  if (!is.vector(columns)) {
+    stop("'columns' argument must be a character vector, not a [", paste0(class(columns), collapse = "/"), "]")
+  }
+
+  if (!is.character(columns)) {
+    stop("'columns' argument must be a character vector")
+  }
+
+  if (anyDuplicated(columns) > 0) {
+    stop("'columns' argument must not contain duplicates")
+  }
+
+  cols_not_found <- setdiff(columns, colnames(.data))
+  if (length(cols_not_found) != 0) {
+    stop("Could not find column/s: [", paste0(cols_not_found, collapse = ", "), "]")
+  }
+
+  if (!is.logical(sort) || length(sort) != 1) {
+    stop("`sort` argument must be TRUE/FALSE, not [", toString(sort), "]")
+  }
+
+  if (!is.character(name)) {
+    stop("`name` argument must be a string, not an object of class [", toString(class(name)), "]")
+  }
+
+  if (length(name) != 1) {
+    stop("`name` argument  must have length of 1, not [", length(name), "]")
+  }
+
+  # Ensure tally column name does NOT
+  # clash with columns we're tallying by
+  # adding '.' prefix until all clashes resolve
+  if (name %in% columns) {
+    name <- prefix_dots_until_not_overlapping(name, taken = columns)
+  }
+
+  # Tally unique combinations
+  results <- as.data.frame(
+    table(.data[, columns, drop = FALSE], useNA = "ifany"),
+    stringsAsFactors = FALSE
+  )
+
+  # Rename Last Column to name
+  colnames(results)[ncol(results)] <- name
+
+  if (sort) {
+    # Sort by tally (descending) if sort = TRUE
+    results <- results[order(-results[[name]]), , drop = FALSE]
+  } else {
+    # Sort by keys (ascending) if sort = FALSE
+    results <- results[do.call(order, results[columns]), , drop = FALSE]
+  }
+
+  # Fix Key Column Types (convert back to original class)
+  # (since table + as.data.frame either converts keys to factors or strings)
+  for (col in columns) {
+    results[[col]] <- convert_vector_to_match_target(results[[col]], .data[[col]], failure = "keep_original")
+  }
+
+  # If drop=TRUE remove zero counts
+  # (produced from factors not present in data)
+  if (drop) {
+    results <- results[results[[name]] > 0, , drop = FALSE]
+  }
+
+  # Ensure rownames are empty
+  rownames(results) <- NULL
+
+  # Add attribute describing true tally count name (see @details)
+  attr(results, "n") <- name
+
+  # Return Result
+  return(results)
+}
+
+
+prefix_dots_until_not_overlapping <- function(string, taken) {
+  while (string %in% taken) {
+    string <- paste0(".", string)
+  }
+
+  return(string)
 }
---
+++ tests/testthat/test-count.R
@@ -0,0 +1,77 @@
+test_that("count counts single column", {
+  df <- data.frame(x = c(2, 1, 1, 2, 1))
+  out <- count(df, "x")
+  expect_equal(out, data.frame(x = c(1, 2), n = c(3, 2)), ignore_attr = TRUE)
+})
+
+test_that("count counts multi columns", {
+  df <- data.frame(x = c(2, 1, 1, 2, 1), y = c("A", "A", "A", "B", "B"))
+  out <- count(df, c("x", "y"))
+
+  expected <- data.frame(
+    x = c(1, 1, 2, 2),
+    y = c("A", "B", "A", "B"),
+    n = c(2, 1, 1, 1)
+  )
+
+  expected <- dplyr::count(df, x, y)
+  expect_equal(out, expected, ignore_attr = TRUE)
+})
+
+test_that("count outputs keys in same type input original data.frame", {
+  # Test Factors
+  df <- data.frame(x = as.factor(c(2, 1, 1, 2, 1)))
+  out <- count(df, "x")
+  expect_true(is.factor(out$x))
+  expect_equal(levels(out$x), c("1", "2"))
+
+  # Test Characters
+  df <- data.frame(x = as.character(c(2, 1, 1, 2, 1)))
+  out <- count(df, "x")
+  expect_true(is.character(out$x))
+
+  # Test Date
+  df <- data.frame(x = as.Date(c(2, 1, 1, 2, 1)))
+  out <- count(df, "x")
+  expect_s3_class(out$x, "Date")
+})
+
+test_that("count includes NA values", {
+  df <- data.frame(x = c(2, 1, 1, 2, NA))
+  out <- count(df, "x")
+  expected <- data.frame(x = c(1, 2, NA), n = c(2, 2, 1))
+
+  expect_equal(out, expected, ignore_attr = TRUE)
+})
+
+test_that("output includes empty levels with drop = FALSE", {
+  df <- data.frame(x = factor("b", levels = c("a", "b", "c")))
+  out <- count(df, "x", drop = FALSE)
+
+  expect_equal(out$n, c(0, 1, 0))
+  expect_equal(out$x, as.factor(c("a", "b", "c")))
+})
+
+test_that("output excludes empty levels with drop = TRUE", {
+  df <- data.frame(x = factor("b", levels = c("a", "b", "c")))
+  out <- count(df, "x", drop = TRUE)
+
+  expect_equal(out$n, c(1))
+  expect_equal(out$x, factor("b", levels = c("a", "b", "c")))
+})
+
+
+test_that("count adds 'n' attribute describing column name", {
+  df <- data.frame(x = c(2, 1, 1, 2, 1))
+  out <- count(df, "x")
+  expected <- data.frame(x = c(1, 2), n = c(3, 2))
+
+  attr(expected, "n") <- "n"
+  expect_equal(out, expected, ignore_attr = FALSE)
+
+  # When name argument is supplied the n attribute changes
+  out2 <- count(df, "x", name = "bob")
+  expected2 <- data.frame(x = c(1, 2), bob = c(3, 2))
+  attr(expected2, "n") <- "bob"
+  expect_equal(out2, expected2, ignore_attr = FALSE)
+})
